99f0eace008a16caacdc6da0f0dce24e17421d8f
zakria-devsinc <muhammad.zakria3637@gmail.com>
2022-05-28 02:42:09 +0500

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const db = require("./db");
 
 require("dotenv").config();
 
-
 let urlEncodedParser = bodyParser.urlencoded({
   extended: true,
 });
@@ -31,7 +30,9 @@ db.mongoose
     process.exit();
   });
 
-app.get("/", (req, res) => {res.send({message:"Welcome to my application"})});
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to my application" });
+});
 //users routes
 require("./users/user.routes")(app);
 // posts routes

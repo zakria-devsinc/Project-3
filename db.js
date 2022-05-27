c26942074775ef0d282b71f446dee5852a1e1f9a
zakria-devsinc <muhammad.zakria3637@gmail.com>
2022-05-28 04:00:18 +0500

    make it better

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = process.env.MONGODB_URL || "mongodb://localhost:27017/posts";
 db.users = require("./users/users.model");
 db.posts = require("./posts/posts.model");
 module.exports = db;
